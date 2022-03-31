42e90635705357fb67300ecc4b0e589c1fe1cc4b
miaoyongbin <miaoyongbin@B-P60DMD6R-2039.local>
2022-03-31 17:19:22 +0800

    spark new

---
+++ src/main/java/com/base/algo/Test.java
@@ -0,0 +1,16 @@
+package com.base.algo;
+
+import java.util.ArrayList;
+
+public class Test {
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<>(10);
+        strings.add("aa");
+
+
+        System.out.println(3^5);
+        System.out.println(3^3);
+
+
+    }
+}
---
+++ src/main/scala/test.scala
@@ -0,0 +1,20 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+object test {
+
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setAppName("test").setMaster("local[*]")
+    val sc = new SparkContext(sparkConf)
+
+    val tuples =
+//      sc.textFile(ClassLoader.getSystemResource("word.csv").getPath)
+      sc.textFile("src/main/resources/word.csv")
+      .flatMap(_.split(","))
+      .map((_, 1))
+      .reduceByKey(_ + _)
+      .collect()
+
+    tuples.foreach(println)
+  }
+
+}
